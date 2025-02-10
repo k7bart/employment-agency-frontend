07e53c5435a0c647b052a011b77af67aec8aaa8c
Kateryna Bartienieva <k7bart@gmail.com>
2025-02-10 13:14:29 +0200

    rewrite submit button using angular material

--- src/app/components/ui/submit-button/submit-button.component.ts
+++ src/app/components/ui/submit-button/submit-button.component.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'submit-button',
-  imports: [],
-  template: '<button type="submit">{{ text() }}</button>',
+  imports: [MatButtonModule],
+  template: '<button mat-button type="submit">{{ text() }}</button>',
   styleUrl: './submit-button.component.css',
 })
 export class SubmitButtonComponent {
