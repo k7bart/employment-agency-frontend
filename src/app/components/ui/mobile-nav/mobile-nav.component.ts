9eedc3aa418c137eb36c72ae1e550f9d867834e8
Kateryna Bartienieva <k7bart@gmail.com>
2025-02-10 16:34:59 +0200

    create mobile-nav component

---
+++ src/app/components/ui/mobile-nav/mobile-nav.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mobile-nav',
+  imports: [],
+  templateUrl: './mobile-nav.component.html',
+  styleUrl: './mobile-nav.component.css',
+})
+export class MobileNavComponent {}
