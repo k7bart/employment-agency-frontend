a258f14e540e1d24d6811a2d0a9674328e761798
Kateryna Bartienieva <k7bart@gmail.com>
2025-02-10 17:29:20 +0200

    create card-details component

---
+++ src/app/components/ui/card-details/card-details.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'card-details',
+  imports: [],
+  template: '<div><ng-content></ng-content></div>',
+  styleUrl: './card-details.component.css',
+})
+export class CardDetailsComponent {}
