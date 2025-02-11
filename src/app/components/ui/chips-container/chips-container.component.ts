8215c0a455ed8f03cc886d3d38cbc579d07c0ab2
Kateryna Bartienieva <k7bart@gmail.com>
2025-02-11 22:20:36 +0200

    create chips-container component

---
+++ src/app/components/ui/chips-container/chips-container.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'chips-container',
+  imports: [],
+  template: '<div><ng-content></ng-content></div>',
+  styleUrl: './chips-container.component.css',
+})
+export class ChipsContainerComponent {}
