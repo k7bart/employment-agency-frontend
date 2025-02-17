f546f1413505496980c5765738830996a3ec5404
Kateryna Bartienieva <k7bart@gmail.com>
2025-02-17 14:51:22 +0200

    refactor agreements component

--- src/app/components/agreements/agreements.component.ts
+++ src/app/components/agreements/agreements.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+
 import { MatDividerModule } from '@angular/material/divider';
 
 import { Agreement } from '../../models/agreement.model';
@@ -7,27 +10,31 @@ import { AgreementsService } from '../../services/agreements.service';
 import { AgreementCardComponent } from './agreement-card/agreement-card.component';
 import { AsideButtonComponent } from '../ui/aside-button/aside-button.component';
 import { AsideComponent } from '../ui/aside/aside.component';
+import { H2TitleComponent } from '../ui/h2-title/h2-title.component';
 import { TitleComponent } from '../ui/title/title.component';
 
 @Component({
   selector: 'app-agreements',
   imports: [
+    AsyncPipe,
+
     MatDividerModule,
 
     AgreementCardComponent,
     AsideComponent,
     AsideButtonComponent,
+    H2TitleComponent,
     TitleComponent,
   ],
   templateUrl: './agreements.component.html',
   styleUrl: './agreements.component.css',
 })
 export class AgreementsComponent implements OnInit {
-  agreements: Agreement[] = [];
+  agreements$!: Observable<Agreement[]>;
 
   constructor(public agreementsService: AgreementsService) {}
 
   ngOnInit() {
-    this.agreements = this.agreementsService.getAgreements();
+    this.agreements$ = this.agreementsService.getAgreements();
   }
 }
