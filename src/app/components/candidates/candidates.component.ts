216df0868c1483d982164826e0daec09885b97d8
Kateryna Bartienieva <k7bart@gmail.com>
2025-02-17 12:36:00 +0200

    set up getting candidates from the service, refactor

--- src/app/components/candidates/candidates.component.ts
+++ src/app/components/candidates/candidates.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+
 import { MatDividerModule } from '@angular/material/divider';
 
 import { CandidatesService } from '../../services/candidates.service';
@@ -7,27 +10,31 @@ import { Candidate } from '../../models/candidate.model';
 import { AsideComponent } from '../ui/aside/aside.component';
 import { AsideButtonComponent } from '../ui/aside-button/aside-button.component';
 import { CandidateCardComponent } from './candidate-card/candidate-card.component';
+import { H2TitleComponent } from '../ui/h2-title/h2-title.component';
 import { TitleComponent } from '../ui/title/title.component';
 
 @Component({
   selector: 'app-candidates',
   imports: [
+    AsyncPipe,
+
     MatDividerModule,
 
     AsideComponent,
     AsideButtonComponent,
     CandidateCardComponent,
+    H2TitleComponent,
     TitleComponent,
   ],
   templateUrl: './candidates.component.html',
   styleUrl: './candidates.component.css',
 })
 export class CandidatesComponent implements OnInit {
-  candidates: Candidate[] = [];
+  candidates$!: Observable<Candidate[]>;
 
   constructor(public candidatesService: CandidatesService) {}
 
   ngOnInit() {
-    this.candidates = this.candidatesService.getCandidates();
+    this.candidates$ = this.candidatesService.getCandidates();
   }
 }
