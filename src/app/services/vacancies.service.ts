edd92b96cc8a650a9b0d12626506e98d259b18ac
Kateryna Bartienieva <k7bart@gmail.com>
2025-02-17 12:38:25 +0200

    add vacancies service

---
+++ src/app/services/vacancies.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Vacancy } from '../models/vacancy.model';
+
+@Injectable({ providedIn: 'root' })
+export class VacanciesService {
+  constructor(private http: HttpClient) {}
+
+  getVacancies() {
+    return this.http.get<Vacancy[]>('http://localhost:8080/vacancies');
+  }
+
+  getVacancyById(id: Vacancy['_id']) {
+    return this.http.get<Vacancy>(`http://localhost:8080/vacancies/${id}`);
+  }
+
+  addVacancy(vacancy: Vacancy) {
+    return this.http.post<Vacancy>('http://localhost:8080/vacancies', vacancy);
+  }
+}
