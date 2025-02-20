6de8ab0f33f6bb995668e327595d92c23e615f5b
Kateryna Bartienieva <k7bart@gmail.com>
2025-02-20 14:48:34 +0200

    expand auth service functionality

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,16 +1,61 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal, computed, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { User } from '../models/user.model';
+import { Router } from '@angular/router';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  private token?: string;
+  readonly isLoggedIn = computed(() => !!this.token());
 
-  constructor(private http: HttpClient) {}
+  private readonly http = inject(HttpClient);
+  private readonly router = inject(Router);
 
-  getToken() {
-    return this.token;
+  private token = signal<string | null>(null);
+  private tokenTimer: ReturnType<typeof setTimeout> | null = null;
+
+  private clearAuthData() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+  }
+
+  private getAuthData() {
+    const token = localStorage.getItem('token');
+    const expirationDate = localStorage.getItem('expirationDate');
+
+    if (!token || !expirationDate) {
+      return null;
+    }
+
+    return {
+      token,
+      expirationDate: new Date(expirationDate),
+    };
+  }
+
+  private saveAuthData(token: string, expirationDate: Date) {
+    localStorage.setItem('token', token);
+    localStorage.setItem('expirationDate', expirationDate.toISOString());
+  }
+
+  private setAuthTimer(duration: number) {
+    this.tokenTimer = setTimeout(() => {
+      this.logout();
+    }, duration * 1000);
+  }
+
+  autoAuthUser() {
+    const authData = this.getAuthData();
+
+    if (!authData) return;
+
+    const expiresIn = authData.expirationDate.getTime() - Date.now();
+
+    if (expiresIn) {
+      this.token.set(authData.token);
+
+      this.setAuthTimer(expiresIn / 1000);
+    }
   }
 
   createUser(email: User['email'], password: User['password']) {
@@ -20,16 +65,44 @@ export class AuthService {
     });
   }
 
+  getToken() {
+    return this.token();
+  }
+
   login(email: User['email'], password: User['password']) {
     this.http
-      .post<{ token: string }>('http://localhost:8080/auth/login', {
-        email,
-        password,
-      })
-
+      .post<{ token: string; expiresIn: number }>(
+        'http://localhost:8080/auth/login',
+        {
+          email,
+          password,
+        },
+      )
       .subscribe((response) => {
-        const { token } = response;
-        this.token = token;
+        const { token, expiresIn } = response;
+
+        if (token) {
+          this.token.set(token);
+
+          this.setAuthTimer(expiresIn);
+
+          const expirationDate = new Date(Date.now() + expiresIn * 1000);
+          this.saveAuthData(token, expirationDate);
+
+          this.router.navigate(['/']);
+        }
       });
+  }
+
+  logout() {
+    this.token.set(null);
+
+    if (this.tokenTimer !== null) {
+      clearTimeout(this.tokenTimer);
+    }
+
+    this.clearAuthData();
+
+    this.router.navigate(['/']);
   }
 }
