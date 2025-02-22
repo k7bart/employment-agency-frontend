7233e33f0b8dab6ed8c47eb576684a0a9176fc27
Kateryna Bartienieva <k7bart@gmail.com>
2025-02-22 08:43:10 +0200

    add areas service

---
+++ src/app/services/areas.service.ts
@@ -0,0 +1,21 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Area } from '../models/area.model';
+
+@Injectable({ providedIn: 'root' })
+export class AreasService {
+  private readonly http = inject(HttpClient);
+
+  getAreas() {
+    return this.http.get<Area[]>('http://localhost:8080/areas');
+  }
+
+  getAreaById(id: Area['_id']) {
+    return this.http.get<Area>(`http://localhost:8080/areas/${id}`);
+  }
+
+  addArea(area: Area) {
+    return this.http.post<Area>('http://localhost:8080/areas', area);
+  }
+}
