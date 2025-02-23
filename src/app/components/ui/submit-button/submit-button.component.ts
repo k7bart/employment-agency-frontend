30333727b0b98a2138c3e76ca65c468d8d138b00
Kateryna Bartienieva <k7bart@gmail.com>
2025-02-23 16:55:13 +0200

    disable submit button if form is invalid

--- src/app/components/ui/submit-button/submit-button.component.ts
+++ src/app/components/ui/submit-button/submit-button.component.ts
@@ -4,9 +4,11 @@ import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-submit-button',
   imports: [MatButtonModule],
-  template: '<button mat-button type="submit">{{ text() }}</button>',
+  template:
+    '<button mat-stroked-button [disabled]="disabled()" type="submit">{{ text() }}</button>',
   styleUrl: './submit-button.component.css',
 })
 export class SubmitButtonComponent {
   text = input<string>('Submit');
+  disabled = input<boolean>(false);
 }
